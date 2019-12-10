1193b9c71b116cd89f9b1b582d8a1d277ee80c65
John Linnane <41014182+panisterfatheroy@users.noreply.github.com>
2019-12-10 15:45:45 +0000

    Add files via upload

--- form-to-email.php
+++ form-to-email.php
@@ -1,146 +1,170 @@
+
+
+
+
 <?php
 
+############################################# SET DEFAULT TEST FOR FIELDS #############################################
 
-if(!isset($_POST['submit']))
-{
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
+$postData = $uploadedFile = $statusMsg = '';
+$msgClass = 'errordiv';
+
+
+############################################# GET FORM DATA #############################################
+
+if(isset($_POST['submit'])){
+    // Get the submitted form data as php variables
+    $postData = $_POST;
+    $type = $_POST['type'];
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $sliderHidden = $_POST['sliderHidden'];
+    $coordHidden = $_POST['coordHidden'];
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+
+
+
+############################################# TEST FORM DATA #############################################
+
+    // Check whether submitted data is not empty
+    if(!empty( $visitor_email) && !empty($name) && !empty($title) && !empty($description)){
+
+        // Validate email
+        if(filter_var( $visitor_email, FILTER_VALIDATE_EMAIL) === false){
+            $statusMsg = 'Please enter your valid email.';
+        }else{
+            $uploadStatus = 1;
+
+############################################# UPLOAD FILE #############################################
+
+            // Upload attachment file
+            if(!empty($_FILES["attachment"]["name"])){
+
+                // File path config
+                $targetDir = "uploads/";
+                $fileName = basename($_FILES["attachment"]["name"]);
+                $targetFilePath = $targetDir . $fileName;
+                $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+
+                // Allow certain file formats
+                $allowTypes = array('pdf', 'doc', 'docx', 'jpg', 'png', 'jpeg');
+                if(in_array($fileType, $allowTypes)){
+                    // Upload file to the server
+                    if(move_uploaded_file($_FILES["attachment"]["tmp_name"], $targetFilePath)){
+                        $uploadedFile = $targetFilePath;
+                    }else{
+                        $uploadStatus = 0;
+                        $statusMsg = "Sorry, there was an error uploading your file.";
+                    }
+                }else{
+                    $uploadStatus = 0;
+                    $statusMsg = 'Sorry, only PDF, DOC, JPG, JPEG, & PNG files are allowed to upload.';
+                }
+            }
+
+############################################# SEND EMAIL IF SUCCESSFUL #############################################
+
+
+            if($uploadStatus == 1){
+
+                // Recipient
+                $toEmail = 'panisterfatheroy@gmail.com';
+                // Sender
+                $from = 'info@travellercollection.com';
+                $fromName = 'TravellerColleciton';
+                // Subject
+                $emailSubject = 'New Site Submitted by '.$name;
+                // Message
+                $htmlContent = '<h2>New Site Submitted</h2>
+                    <p><b>Type:</b><br/>'.$type.'</p>
+                    <p><b>Title:</b> '.$title.'</p>
+                    <p><b>Description:</b><br/>'.$description.'</p>
+                    <p><b>Time:</b><br/>'.$sliderHidden.'</p>
+                    <p><b>Coordinates:</b><br/>'.$coordHidden.'</p>
+                    <p><b>Name:</b> '.$name.'</p>
+                    <p><b>Email:</b> '. $visitor_email.'</p>';
+                // Header for sender info
+                $headers = "From: $fromName"." <".$from.">";
+
+############################################# ID THE FILE #############################################
+
+
+                if(!empty($uploadedFile) && file_exists($uploadedFile)){
+
+                    // Boundary
+                    $semi_rand = md5(time());    // generates Message Digest Algorithm hash
+                    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
+
+                    // Headers for attachment
+                    $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\"";
+
+                    // Multipart boundary
+                    $description = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
+                    "Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n";
+
+############################################# ATTACH FILE #############################################
+
+                    // Preparing attachment
+                    if(is_file($uploadedFile)){
+                        $description .= "--{$mime_boundary}\n";
+                        $fp =    @fopen($uploadedFile,"rb");
+                        $data =  @fread($fp,filesize($uploadedFile));
+                        @fclose($fp);
+                        $data = chunk_split(base64_encode($data));
+                        $description .= "Content-Type: application/octet-stream; name=\"".basename($uploadedFile)."\"\n" .
+                        "Content-Description: ".basename($uploadedFile)."\n" .
+                        "Content-Disposition: attachment;\n" . " filename=\"".basename($uploadedFile)."\"; size=".filesize($uploadedFile).";\n" .
+                        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+                    }
+
+                    $description .= "--{$mime_boundary}--";
+                    $returnpath = "-f" .  $visitor_email;
+
+                    // Send email
+                    $mail = mail($toEmail, $emailSubject, $description, $headers, $returnpath);
+
+                    // Delete attachment file from the server
+                    // @unlink($uploadedFile);
+                }else{
+                     // Set content-type header for sending HTML email
+                    $headers .= "\r\n". "MIME-Version: 1.0";
+                    $headers .= "\r\n". "Content-type:text/html;charset=UTF-8";
+
+                    // Send email
+                    $mail = mail($toEmail, $emailSubject, $htmlContent, $headers);
+                }
+
+############################################# SUCCESS MESSAGE #############################################
+
+
+                // If mail sent
+                if($mail){
+                    $statusMsg = 'Your contact request has been submitted successfully !';
+                    $msgClass = 'succdiv';
+
+                    $postData = '';
+                }else{
+                    $statusMsg = 'Your contact request submission failed, please try again.';
+                }
+            }
+        }
+    }else{
+        $statusMsg = 'Please fill all the fields.';
+    }
 }
-
-
-//********************* DEFINE VARIABLES FROM FORM VALUES ******************
-
-$type = $_POST['type'];
-$title = $_POST['title'];
-$description = $_POST['description'];
-$sliderHidden = $_POST['sliderHidden'];
-$coordHidden = $_POST['coordHidden'];
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-
-print_r($_POST);
-
-//********************* DEFINE VARIABLES FOR FILE TRANSFER ******************
-
-
-$target_dir = "uploads/"; // directory where the file is going
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]); // path of the file to be uploaded
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION)); // file extension of the file
-
-//********************* FILE CHECKS ******************
-
-
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-      echo "File is an image - " . $check["mime"] . ".";
-      $uploadOk = 1;
-  } else {
-      echo "File is not an image.";
-      $uploadOk = 0;
-  }
-}
-
-
-if (file_exists($target_file)) { // Check if file already exists
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
-
-
-// if ($_FILES["fileToUpload"]["size"] > 500000) {  // Check file size
-//   echo "Sorry, your file is too large.";
-//   $uploadOk = 0;
-// }
-
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-      echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-  } else {
-      echo "Sorry, there was an error uploading your file.";
-  }
-}
-
-
-//********************* CHECK FORM IS FILLED OUT OK ******************
-
-
-//Validate first
-if(empty($name)||empty($visitor_email))
-{
-    echo "Name and email are mandatory!";
-    exit;
-}
-
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
-
-
-//********************* COMPOSE EMAIL ******************
-
-
-$email_from = 'info@travellercollection.com';
-$email_subject = "Traveller Collection: New Site Submission";
-$email_body = 
-    "TYPE: $type\n".
-    "TITLE: $title\n".
-    "DESCRIPTION: $description\n".
-    "ASSOCIATED TIME: $sliderHidden\n".
-    "LOCATION: $coordHidden\n".
-    "SUBMITTER'S NAME: $name.\n".
-    "SUBMITTER'S EMAIL: $visitor_email.\n";
-
-
-$to = "panisterfatheroy@gmail.com";
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-
-// ********************* OPEN THANK YOU PAGE  ******************
-
-// header('Location: thank-you.html');
-
-// ********************* JAVASCRIPT FORM VALIDATION FUNCTIONS ******************
-
-
-// Function to validate against any email injection attempts
-function IsInjected($str)
-{
-  $injections = array('(\n+)',
-              '(\r+)',
-              '(\t+)',
-              '(%0A+)',
-              '(%0D+)',
-              '(%08+)',
-              '(%09+)'
-              );
-  $inject = join('|', $injections);
-  $inject = "/$inject/i";
-  if(preg_match($inject,$str))
-    {
-    return true;
-  }
-  else
-    {
-    return false;
-  }
-}
-
 ?>
+
+<!-- *********************** INTRO CODE *************************** -->
+
+
+    <!-- Display submission status -->
+<?php if(!empty($statusMsg)){ ?>
+    <p class="statusMsg <?php echo !empty($msgClass)?$msgClass:''; ?>"><?php echo $statusMsg; ?></p>
+<?php } ?>
+
+
+
+
+  </body>
+</html>
